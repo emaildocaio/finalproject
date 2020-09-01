8d9187c6bfb787af864bdeaa2db7784827c63354
Vinicius Moraes <vmrc@ding-an-sich.com>
2020-09-01 17:19:07 -0300

    Add product show as guest and relevant pundit policy

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,9 +1,13 @@
 class ProductsController < ApplicationController
+  skip_before_action :authenticate_user!, only: %i[index show]
+
   def index
     @products = Product.all
   end
 
   def show
+    @product = Product.find(params[:id])
+    authorize @product
   end
 
   def new
