7966bb6c9ff6e2f139b0e2031ad0f485ada59b68
Caio Farias <emaildocaio@gmail.com>
2020-09-01 12:31:36 -0300

    ShoppingCart model Create

---
+++ app/models/shopping_cart.rb
@@ -0,0 +1,8 @@
+class ShoppingCart < ApplicationRecord
+  belongs_to :user
+
+  validates :voucher, :total_price, :status, :user_id, presence: true
+  validates :voucher, uniqueness: true
+  validates :status, inclusion: { in: %w(pendente cancelado pago),
+    message: "%{value} não é um status válido!" }
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_31_192720) do
+ActiveRecord::Schema.define(version: 2020_09_01_141820) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "shopping_carts", force: :cascade do |t|
+    t.string "voucher"
+    t.string "status"
+    t.float "total_price"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_shopping_carts_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "first_name", null: false
@@ -32,4 +42,5 @@ ActiveRecord::Schema.define(version: 2020_08_31_192720) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "shopping_carts", "users"
 end
