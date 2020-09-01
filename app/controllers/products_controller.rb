d0509c00601f94c81d7984dcf1d00a67e9b2d237
Caio Farias <emaildocaio@gmail.com>
2020-09-01 18:04:10 -0300

    Products Routes and Controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,6 @@
 class ProductsController < ApplicationController
+  before_action :set_product, only: [:edit, :update, :show, :destroy]
+
   def index
     @products = Product.all
   end
@@ -13,11 +15,30 @@ class ProductsController < ApplicationController
   end
 
   def edit
+    authorize @product
   end
 
   def update
+    authorize @product
+    @product.update(product_params)
+
+    if @product.save
+      redirect_to product_path
+    else
+      render :edit
+    end
   end
 
   def destroy
+  end
+
+  private
+
+  def set_product
+    @product = Product.find(params[:id])
+  end
+
+  def product_params
+    params.require(:product).permit(:name, :price, :activity, :capacity, :status)
   end
 end
---
+++ app/policies/product_policy.rb
@@ -0,0 +1,21 @@
+class ProductPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope.all
+    end
+  end
+
+    def edit?
+      record.company.user == user
+    # - record: the restaurant passed to the `authorize` method in controller
+    # - user:   the `current_user` signed in with Devise.
+    end
+
+    def update?
+      edit?
+      # - record: the restaurant passed to the `authorize` method in controller
+    # - user:   the `current_user` signed in with Devise.
+    end
+
+
+end
