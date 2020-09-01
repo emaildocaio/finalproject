f1d63f6c6264af91924ee5f8f99cd7d949fefd62
Caio Farias <emaildocaio@gmail.com>
2020-09-01 14:14:22 -0300

    Trying to solve conflicts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_01_161206) do
+ActiveRecord::Schema.define(version: 2020_09_01_161523) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,16 @@ ActiveRecord::Schema.define(version: 2020_09_01_161206) do
     t.index ["company_id"], name: "index_products_on_company_id"
   end
 
+  create_table "shopping_carts", force: :cascade do |t|
+    t.string "voucher"
+    t.string "status"
+    t.integer "total_price"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_shopping_carts_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "first_name", null: false
     t.string "last_name", null: false
@@ -55,4 +65,5 @@ ActiveRecord::Schema.define(version: 2020_09_01_161206) do
 
   add_foreign_key "companies", "users"
   add_foreign_key "products", "companies"
+  add_foreign_key "shopping_carts", "users"
 end
