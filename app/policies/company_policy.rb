5fd1b2ae3c3b842c99ccfcddaeb7ad0e864aeab3
Renata Grassi <renagrassi@gmail.com>
2020-09-01 15:24:40 -0300

    installed pundit and tested with company model

---
+++ app/policies/company_policy.rb
@@ -0,0 +1,19 @@
+class CompanyPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope.all
+    end
+  end
+
+  def new?
+    true
+  end
+
+  def create?
+    true
+  end
+
+  def show?
+    record.user == user
+  end
+end
