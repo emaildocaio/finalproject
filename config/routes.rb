b679de028a43526bcf9aaf481b2bfe877eb3c89b
Vinicius Moraes <vmrc@ding-an-sich.com>
2020-09-02 14:09:57 -0300

    Bookings, first commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   devise_for :users
   root to: 'pages#home'
   resources :companies
-  resources :products
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :products do
+    resources :bookings, only: %i[new create]
+  end
   get '/myproducts', to: 'products#my_index'
-
 end
