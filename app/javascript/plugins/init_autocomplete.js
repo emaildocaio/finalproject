0a09be9c31b89b03ed28312e415b585a52ff8392
Caio Farias <emaildocaio@gmail.com>
2020-09-08 15:06:38 -0300

    Implemented Google Location, Maps and Autocomplete

--- app/javascript/plugins/init_autocomplete.js
+++ app/javascript/plugins/init_autocomplete.js
@@ -1,10 +1,27 @@
-import places from 'places.js';
+// import places from 'places.js';
 
-const initAutocomplete = () => {
-  const addressInput = document.getElementById('address-new-company');
-  if (addressInput) {
-    places({ container: addressInput });
-  }
-};
+// const initAutocomplete = () => {
+//   const addressInput = document.getElementById('address-new-company');
+//   if (addressInput) {
+//     places({ container: addressInput });
+//   }
+// };
+
+// export { initAutocomplete };
+
+
+
+function initAutocomplete() {
+    var companyAddress = document.getElementById('address-new-company');
+
+    if (companyAddress) {
+      var initAutocomplete = new google.maps.places.Autocomplete(companyAddress, { types: [ 'geocode' ] });
+      google.maps.event.addDomListener(companyAddress, 'keydown', function(e) {
+        if (e.key === "Enter") {
+          e.preventDefault(); // Do not submit the form on Enter.
+        }
+      });
+    }
+}
 
 export { initAutocomplete };
