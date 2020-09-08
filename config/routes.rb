1d0ba9342820ea64518e10b76b6b9b9174ae0582
Caio Farias <64873438+emaildocaio@users.noreply.github.com>
2020-09-08 16:44:01 -0300

    Apagando linhas desnecessárias
    Apagando as duas linhas que são criadas automaticamente no routes quando gera um modelo por helper

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  get 'guests/index'
-  get 'guests/create'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
   devise_for :users
   root to: 'pages#home'
   resources :companies
