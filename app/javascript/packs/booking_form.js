9ed1955684f3c65049a9623d83c34d6e83667866
Renata Grassi <renagrassi@gmail.com>
2020-09-09 18:39:23 -0300

    adding bookin_form

--- app/javascript/packs/booking_form.js
+++ app/javascript/packs/booking_form.js
@@ -1,36 +1,66 @@
-const minusBtn = document.querySelector('.minus');
-const plusBtn = document.querySelector('.actions a:last-child');
-const bookingCount = document.getElementById('booking_participants');
+// const minusBtn = document.querySelector('.minus');
+// const plusBtn = document.querySelector('.actions a:last-child');
+// const bookingCount = document.getElementById('booking_participants');
 
 
-const updateValues = (counterN) => {
-  const span_price_element = document.getElementById('product-price')
-  const unit_price = parseInt(span_price_element.innerText, 10)
-  const total_price = counterN * unit_price;
-  document.getElementById('price-input').innerText = `Total R$ ${total_price}`;
-};
+// const updateValues = (counterN) => {
+//   const span_price_element = document.getElementById('product-price')
+//   const unit_price = parseInt(span_price_element.innerText, 10)
+//   const total_price = counterN * unit_price;
+//   document.getElementById('price-input').innerText = `Total R$ ${total_price}`;
+// };
 
-const updateMinusBtn = (counterN) => {
-  if (counterN > 1) {
-    minusBtn.setAttribute('href', 'javascript:void(0)');
-  } else minusBtn.removeAttribute('href');
-};
+// const updateMinusBtn = (counterN) => {
+//   if (counterN > 1) {
+//     minusBtn.setAttribute('href', 'javascript:void(0)');
+//   } else minusBtn.removeAttribute('href');
+// };
 
-const changeCounter = (event) => {
-  const counter = document.getElementById('counter');
-  let counterN = parseInt(counter.dataset.count, 10);
-  counterN += parseInt(event.currentTarget.dataset.offset, 10);
-  counter.dataset.count = counterN;
-  counter.innerText = counter.dataset.count;
-  updateMinusBtn(counterN);
-  updateValues(counterN);
-  bookingCount.value = counterN
-};
+// const changeCounter = (event) => {
+//   const counter = document.getElementById('counter');
+//   let counterN = parseInt(counter.dataset.count, 10);
+//   counterN += parseInt(event.currentTarget.dataset.offset, 10);
+//   counter.dataset.count = counterN;
+//   counter.innerText = counter.dataset.count;
+//   updateMinusBtn(counterN);
+//   updateValues(counterN);
+//   bookingCount.value = counterN
+// };
 
-plusBtn.addEventListener('click', changeCounter);
+// plusBtn.addEventListener('click', changeCounter);
 
-minusBtn.addEventListener('click', (event) => {
-  if (event.currentTarget.attributes.href) {
-    changeCounter(event);
+// minusBtn.addEventListener('click', (event) => {
+//   if (event.currentTarget.attributes.href) {
+//     changeCounter(event);
+//   }
+// });
+
+
+
+const guestBtn = document.getElementById('add-guest-btn')
+
+
+const guestDynamicPrice = () => {
+  if (guestBtn) {
+    const productPrice = document.getElementById('product-price')
+    const totalPrice = document.getElementById('price-input')
+    let price = parseInt(productPrice.innerText)
+    guestBtn.addEventListener('click', (event) => {
+      event.preventDefault()
+      price += parseInt(productPrice.innerText)
+      document.getElementById('price-input').innerText = `Total R$ ${price}`; 
+    });
+    $('#form-container').on('cocoon:after-remove', function(e, insertedItem, originalEvent) {
+      price -= parseInt(productPrice.innerText)
+      document.getElementById('price-input').innerText = `Total R$ ${price}`; 
+    });
   }
-});
+  
+
+  //guardar numa variaval o preco, e o total
+  // guardar numa variavel o add guest
+  // add event listener no guest, click (soma o valor da trip no total). Depois
+  // achar o último botao remove e botar eventlistener click nele (e subtrai o valor
+  // da trip).
+}
+export { guestDynamicPrice }
