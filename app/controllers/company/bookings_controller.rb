162a789d1bd1d62900e96a179bd9e7e45ee61a16
Vinicius Moraes <vmrc@ding-an-sich.com>
2020-09-10 18:20:12 -0300

    Fixed same date range filtering bug

--- app/controllers/company/bookings_controller.rb
+++ app/controllers/company/bookings_controller.rb
@@ -4,7 +4,12 @@ class Company::BookingsController < ApplicationController
       @bookings = Booking.where(product: current_user.company.products).order(date: :asc)
     else
       @dates = build_dates
-      @bookings = Booking.where(product: current_user.company.products, date: @dates[0]..@dates[2]).order(date: :asc)
+      case @dates.size
+      when 1
+        @bookings = Booking.where(product: current_user.company.products, date: @dates[0]).order(date: :asc)
+      else
+        @bookings = Booking.where(product: current_user.company.products, date: @dates[0]..@dates[2]).order(date: :asc)
+      end
     end
   end
 
