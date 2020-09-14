4ebec62256fecfda7927b5fcceef3a0a9f5274ac
Vinicius Moraes <vmrc@ding-an-sich.com>
2020-09-14 10:04:22 -0300

    Fixed potential duplicate product name bug

--- app/controllers/company/products_controller.rb
+++ app/controllers/company/products_controller.rb
@@ -1,10 +1,11 @@
 class Company::ProductsController < ApplicationController
   def index
+    @products = current_user.company.products
     if params[:search].nil?
-      @bookings = Booking.where(product: current_user.company.products).order(date: :asc)
+      @bookings = Booking.where(product: @products).order(date: :asc)
     else
-     product = Product.find_by(name: params[:search][:product])
-     @bookings = Booking.where(product: product).order(date: :asc)
+      product = Product.find(params[:search][:product])
+      @bookings = Booking.where(product: product).order(date: :asc)
     end
   end
 end
