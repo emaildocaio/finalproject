b1376f30ad08edfed8e12a2e983dea529aca3341
Vinicius Moraes <vmrc@ding-an-sich.com>
2020-09-14 12:01:45 -0300

    Show product title for clarity

--- app/controllers/company/products_controller.rb
+++ app/controllers/company/products_controller.rb
@@ -1,14 +1,15 @@
 class Company::ProductsController < ApplicationController
   def index
     @products = current_user.company.products
+    @product = @products.first
     if params[:search].nil?
-      @bookings = Booking.where(product: @products.first).order(date: :asc)
+      @bookings = Booking.where(product: @product).order(date: :asc)
     else
-      product = Product.find(params[:search][:product])
+      @product = Product.find(params[:search][:product])
       if params[:search][:date].present?
-        @bookings = Booking.where(product: product, date: params[:search][:date]).order(date: :asc)
+        @bookings = Booking.where(product: @product, date: params[:search][:date]).order(date: :asc)
       else
-        @bookings = Booking.where(product: product).order(date: :asc)
+        @bookings = Booking.where(product: @product).order(date: :asc)
       end
     end
   end
