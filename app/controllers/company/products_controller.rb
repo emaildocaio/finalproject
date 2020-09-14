285337a87bb95fa672e0247030d0cb7287915486
Vinicius Moraes <vmrc@ding-an-sich.com>
2020-09-14 11:56:10 -0300

    Filter by trip and date. Simple view

--- app/controllers/company/products_controller.rb
+++ app/controllers/company/products_controller.rb
@@ -2,10 +2,14 @@ class Company::ProductsController < ApplicationController
   def index
     @products = current_user.company.products
     if params[:search].nil?
-      @bookings = Booking.where(product: @products).order(date: :asc)
+      @bookings = Booking.where(product: @products.first).order(date: :asc)
     else
       product = Product.find(params[:search][:product])
-      @bookings = Booking.where(product: product).order(date: :asc)
+      if params[:search][:date].present?
+        @bookings = Booking.where(product: product, date: params[:search][:date]).order(date: :asc)
+      else
+        @bookings = Booking.where(product: product).order(date: :asc)
+      end
     end
   end
 end
