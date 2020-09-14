9ffaeed104741101814b94414c1750263d7495f4
Vinicius Moraes <vmrc@ding-an-sich.com>
2020-09-14 12:15:33 -0300

    Some security without pundit. Rescue from record not found.

--- app/controllers/company/products_controller.rb
+++ app/controllers/company/products_controller.rb
@@ -1,16 +1,24 @@
 class Company::ProductsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+
   def index
     @products = current_user.company.products
     @product = @products.first
     if params[:search].nil?
       @bookings = Booking.where(product: @product).order(date: :asc)
     else
-      @product = Product.find(params[:search][:product])
+      @product = @products.find(params[:search][:product])
       if params[:search][:date].present?
         @bookings = Booking.where(product: @product, date: params[:search][:date]).order(date: :asc)
       else
         @bookings = Booking.where(product: @product).order(date: :asc)
       end
     end
+  end
+
+  private
+
+  def record_not_found
+    redirect_to action: 'index'
   end
 end
