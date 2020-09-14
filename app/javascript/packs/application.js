4beab27100d22fb654894bea0b6825cd4b3297a7
Caio Farias <64873438+emaildocaio@users.noreply.github.com>
2020-09-14 18:02:15 -0300

    Revert "Graphic"

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,8 +9,6 @@ require("@rails/activestorage").start()
 require("channels")
 require("jquery")
 require("@nathanvda/cocoon")
-require("chartkick")
-require("chart.js")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
