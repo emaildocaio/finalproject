71b4654f3b87e915f81b67f5bd486cf9a7e71b39
Caio Farias <emaildocaio@gmail.com>
2020-09-16 22:18:02 -0300

    FAVICON OK

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -32,6 +32,10 @@ class ApplicationController < ActionController::Base
       devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
     end
 
+    def default_url_options
+      { host: ENV["DOMAIN"] || "localhost:3000" }
+    end
+
 end
 
 
